aebd07bb6743ab980ecbdfaf7f8f277d382a7445
vmrfreitas <vmrfreitas@inf.ufrgs.br>
2023-02-25 02:56:28 -0300

    can now play tictactoe, but no winning or losing

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,20 +7,22 @@ public class GameController : MonoBehaviour
     public GameObject game;
     public Sprite xSprite;
     public Sprite oSprite;
+    public bool playerTurn;
 
     public int[,] gameState = new int[3, 3];
     int[] checkValues = new int[5];
     // Start is called before the first frame update
     void Start()
     {
-        var child = game.transform.GetChild(0);   
+        playerTurn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(true) { // if player played
+    }
 
-        }
+    public int checkGameState(){
+        return 0;
     }
 }
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public Vector2Int tileCoord;
+    public SpriteRenderer spriteRenderer;
+    private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = transform.GetComponentInParent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseOver() {
+        Sprite playSprite;
+        int playValue;
+        if(gameController.gameState[tileCoord.x, tileCoord.y]==0){
+            if(Input.GetMouseButtonDown(0)){
+                if(gameController.playerTurn){
+                    playValue = 1;
+                    playSprite = gameController.xSprite;
+                    gameController.playerTurn = false;
+                } else {
+                    playValue = -1;
+                    playSprite = gameController.oSprite;
+                    gameController.playerTurn = true;
+                }
+                gameController.gameState[tileCoord.x, tileCoord.y] = playValue;
+                spriteRenderer.sprite = playSprite;
+            }
+        }
+    }
+}
+
+
