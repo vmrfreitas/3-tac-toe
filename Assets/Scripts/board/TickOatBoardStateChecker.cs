4adb1cd5195e5be7674cd89ae2cd5a988378c442
Vinicius Milani <vmrfreitas@inf.ufrgs.br>
2023-10-17 20:57:29 -0300

    more tickoat foundations

--- Assets/Scripts/board/TickOatBoardStateChecker.cs
+++ Assets/Scripts/board/TickOatBoardStateChecker.cs
@@ -6,6 +6,47 @@ using UnityEngine.SceneManagement;
 public class TickOatBoardStateChecker : BoardStateChecker
 {
     public int check(BoardState boardState, bool forReal) {
+        if(boardState.TurnNum<6){
+            return 0;
+        }
+
+        if(boardState.DiagonalSums[0] == 33 || boardState.DiagonalSums[1] == 33){
+            if(GameOptions.PlayerTurn){ // this is not enough, I need playerTurn on boardState not on gameOptions!
+                if(forReal){
+                    Debug.Log("player won");
+                    GameOptions.GameOver = true;
+                }
+                boardState.gameOver = true;
+                return 1;
+            } else {
+                if(forReal){
+                    Debug.Log("computer won");
+                    GameOptions.GameOver = true;
+                }
+                boardState.gameOver = true;
+                return -1;
+            }
+        }
+
+        for(int i=0; i<3; i++){
+            if(boardState.LineSums[i]==33 || boardState.ColumnSums[i]==33){
+                if(GameOptions.PlayerTurn){
+                    if(forReal){
+                        Debug.Log("player won");
+                        GameOptions.GameOver = true;
+                    }
+                    boardState.gameOver = true;
+                    return 1;
+                } else {
+                    if(forReal){
+                        Debug.Log("computer won");
+                        GameOptions.GameOver = true;
+                    }
+                    boardState.gameOver = true;
+                    return -1;
+                }
+            }
+        }
         return 0;
     }
 }
--- Assets/Scripts/computer/possibleMoves/TickOatMovesCalculator.cs
+++ Assets/Scripts/computer/possibleMoves/TickOatMovesCalculator.cs
@@ -6,16 +6,31 @@ using UnityEngine.SceneManagement;
 public class TickOatMovesCalculator : PossibleMovesCalculator
 {
     public List<(Vector2, int)> calculate(int[,] boardMatrix, bool isMin) {
-        /*
-        List<Vector2> moves = new List<Vector2>();
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(gameMatrix[i,j]==0){
-                    moves.Add(new Vector2(i,j));
+        List<(Vector2, int)> moves = new();
+        int moveValue;
+        if(isMin){
+            moveValue = 10;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    if(boardMatrix[i,j]==1){
+                        moves.Add((new Vector2(i,j), 11));
+                    } else if(boardMatrix[i,j]==0){
+                        moves.Add((new Vector2(i,j), moveValue));
+                    }
+                }
+            }
+        } else {
+            moveValue = 1;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    if(boardMatrix[i,j]==1){
+                        moves.Add((new Vector2(i,j), 11));
+                    } else if(boardMatrix[i,j]==0){
+                        moves.Add((new Vector2(i,j), moveValue));
+                    }
                 }
             }
         }
-        return moves;*/
-        return new List<(Vector2, int)>();
+        return moves;
     }
 }
